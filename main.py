6f9752e0053d6dc9ed6b382f40a8d84aeb5e151e
Sameer Ahmad <samimughal7786@gmail.com>
2024-04-14 21:41:32 +0500

    Data Transformation Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.MLOps_Project import logger
 from src.MLOps_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.MLOps_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.MLOps_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 STAGE_NAME = "Data Ingestion Stage"
 if __name__ == '__main__':
     try:
@@ -18,6 +19,18 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(
+            f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(
             f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
